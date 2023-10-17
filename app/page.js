6faf0dac0738c30888c2a22501d891ebcc300781
ST Distribuidora <caioluan18@gmail.com>
2023-10-16 21:23:54 -0300

    customized hook with dark and light themes created

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-import { ListContainer } from './components/listContainer'
-import styles from './page.module.css'
+import { ListContainer } from "./components/listContainer";
+import styles from "./page.module.css";
+import useTheme from "./hooks/useTheme";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <ListContainer>
-        Todo Github
-      </ListContainer>
+      <ListContainer>Todo Github</ListContainer>
     </main>
-  )
+  );
 }
